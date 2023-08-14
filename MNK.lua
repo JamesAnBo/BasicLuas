40bf3ea145c9790262e01e140911938cf86fbd41
Aesk <aesk.99@gmail.com>
2023-08-14 14:53:48 -0400

    Update MNK.lua

--- MNK.lua
+++ MNK.lua
@@ -5,7 +5,7 @@
 local profile = {};
 
 blinclude = gFunc.LoadFile('common\\blinclude.lua');
---local isTargetTagged = gFunc.LoadFile('common\\isTargetTagged.lua');
+local isTargetTagged = gFunc.LoadFile('common\\isTargetTagged.lua');
 
 --If you change the names here, Make sure to change the weapon sets below to match.
 	--DO NOT CHANGE 'Default'
@@ -302,9 +302,9 @@ profile.HandleDefault = function()
 		if (footwork >= 1) then gFunc.EquipSet(sets.Footwork) end
 		if (blinclude.GetCycle('TH') ~= 'none') then
 			if (blinclude.GetCycle('TH') == 'Tag') then 
-				--if (not isTargetTagged()) then
+				if (not isTargetTagged()) then
 					gFunc.EquipSet(sets.TH);
-				--end
+				end
 			elseif (blinclude.GetCycle('TH') == 'Fulltime') then
 				gFunc.EquipSet(sets.TH);
 			end
